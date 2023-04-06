0ca1b92a2acd01b1bfcdee647daeea87b28aa7df
John Salib <john.salib@teampicnic.com>
2023-04-06 11:25:05 +0200

    Rename Collection to ArtCollection in RijksService

--- app/src/main/java/com/example/myrijks/data/api/RijksService.kt
+++ app/src/main/java/com/example/myrijks/data/api/RijksService.kt
@@ -1,10 +1,10 @@
-package com.example.myrijks.data.model.api
+package com.example.myrijks.data.api
 
-import com.example.myrijks.data.model.Collection
+import com.example.myrijks.data.model.ArtCollection
 import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
 
 interface RijksService {
     @GET("/api/en/collection") // TODO configure culture by language
-    fun getCollection(): Single<Collection>
+    fun getCollection(): Single<ArtCollection>
 }
