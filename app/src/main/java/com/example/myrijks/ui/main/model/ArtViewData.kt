bba72d7b7ba3d214c08367b3262af273b7d8f6be
John Salib <john.salib@teampicnic.com>
2023-04-06 11:11:59 +0200

    Add ArtViewData

---
+++ app/src/main/java/com/example/myrijks/ui/main/model/ArtViewData.kt
@@ -0,0 +1,9 @@
+package com.example.myrijks.ui.main.model
+
+data class ArtViewData(
+    val id: String,
+    val title: String,
+    val objectNumber: String,
+    val imageUrl: String,
+    val principalOrFirstMaker: String
+)
