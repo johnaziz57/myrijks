0d7dd9a00053f2a8045c4a59aa689202bdf241af
John Salib <john.salib@teampicnic.com>
2023-04-06 23:06:07 +0200

    Add AuthenticationInterceptor to Retrofit

--- app/src/main/java/com/example/myrijks/data/di/NetworkModule.kt
+++ app/src/main/java/com/example/myrijks/data/di/NetworkModule.kt
@@ -1,10 +1,12 @@
 package com.example.myrijks.data.di
 
+import com.example.myrijks.data.api.AuthenticationInterceptor
 import com.example.myrijks.data.api.RijksService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -15,11 +17,18 @@ object NetworkModule {
 
     @Provides
     fun providesPlacesService(): RijksService {
+
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(AuthenticationInterceptor())
+            .build()
+
         val retrofit = Retrofit.Builder()
             .baseUrl("https://www.rijksmuseum.nl/")
             .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
             .addConverterFactory(MoshiConverterFactory.create())
+            .client(okHttpClient)
             .build()
+
         return retrofit.create(RijksService::class.java)
     }
 }
