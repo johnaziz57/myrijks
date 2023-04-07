073f6a6e3461e65ac22eb36caf37997a1676ce1d
John Salib <john.salib@teampicnic.com>
2023-04-07 20:07:27 +0200

    Add Test for ArtDataMapper

---
+++ app/src/test/java/com/example/myrijks/domain/mapper/ArtDataMapperTest.kt
@@ -0,0 +1,48 @@
+package com.example.myrijks.domain.mapper
+
+import com.example.myrijks.data.model.ArtObject
+import com.example.myrijks.data.model.Image
+import com.example.myrijks.ui.feature.main.model.ArtViewData
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
+
+@RunWith(MockitoJUnitRunner::class)
+class ArtDataMapperTest {
+
+    private lateinit var artDataMapper: ArtDataMapper
+
+    @Before
+    fun setUp() {
+        artDataMapper = ArtDataMapper()
+    }
+
+    @Test
+    fun `test art data mapper`() {
+        val image = mock<Image> {
+            on { url } doReturn "url"
+        }
+
+        val artObject = mock<ArtObject> {
+            on { id } doReturn "id"
+            on { title } doReturn "title"
+            on { objectNumber } doReturn "objectNumber"
+            on { webImage } doReturn image
+            on { principalOrFirstMaker } doReturn "maker"
+        }
+
+        val artViewData = artDataMapper.mapToArtViewData(artObject)
+        val expectedArtViewData = ArtViewData(
+            "id",
+            "title",
+            "objectNumber",
+            "url",
+            "maker"
+        )
+        assertEquals(artViewData, expectedArtViewData)
+    }
+}
