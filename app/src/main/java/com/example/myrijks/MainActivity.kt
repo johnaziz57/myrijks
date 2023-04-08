5b61022862ab6139980b6c1da3a33829baac58c5
John Salib <john.salib@teampicnic.com>
2023-04-08 10:43:16 +0200

    Add navigation component

--- app/src/main/java/com/example/myrijks/MainActivity.kt
+++ app/src/main/java/com/example/myrijks/MainActivity.kt
@@ -1,20 +1,7 @@
 package com.example.myrijks
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.myrijks.ui.feature.main.MainFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
-    }
-}
+class MainActivity : AppCompatActivity(R.layout.activity_main)
